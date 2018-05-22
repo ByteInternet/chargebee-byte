f1e0e35c63bbc3fb09014eed66069845639eb439
Bono de Visser <bono@byte.nl>
2018-05-22 11:48:05 +0200

    Add get_all_subscriptions function
    Add a function that will get all subscriptions available. Also takes into account the provided parameters.

--- chargebee_byte/client.py
+++ chargebee_byte/client.py
@@ -1,4 +1,5 @@
 import requests
+from copy import deepcopy
 
 from chargebee_byte.requests import SubscriptionRequest
 
@@ -13,3 +14,14 @@ class Client(object):
         response = requests.get(self.api_url + request.path, auth=self.auth, params=request.data)
         response.raise_for_status()
         return response.json()
+
+    def get_all_subscriptions(self, parameters=None):
+        ret = self.get_subscriptions(parameters)
+        subscriptions = ret['list']
+
+        if 'next_offset' in ret:
+            new_parameters = deepcopy(parameters) or {}
+            new_parameters.update({'offset': ret['next_offset']})
+            subscriptions += self.get_all_subscriptions(new_parameters)
+
+        return subscriptions
--- tests/test_client.py
+++ tests/test_client.py
@@ -75,3 +75,122 @@ class TestGetSubscriptions(TestBase):
         self.chargebee_client.get_subscriptions()
 
         response.raise_for_status.assert_called_once_with()
+
+
+class TestGetAllSubscriptions(TestBase):
+    def setUp(self):
+        super(TestGetAllSubscriptions, self).setUp()
+        self.requests_get = self.set_up_patch('chargebee_byte.client.requests.get')
+
+        self.response = mock.Mock()
+        self.response.json.return_value = {'list': []}
+        self.requests_get.return_value = self.response
+
+    def test_raises_value_error_if_unknown_parameter_supplied(self):
+        parameter = 'banaan[henk]'
+        self.assertNotIn(parameter, SubscriptionRequest().allowed_parameters)
+
+        with self.assertRaises(ValueError):
+            self.chargebee_client.get_all_subscriptions(parameters={parameter: 'bonobo'})
+
+    def test_calls_raise_for_status_on_requests_response(self):
+        self.chargebee_client.get_all_subscriptions()
+
+        self.response.raise_for_status.assert_called_once_with()
+
+    def test_keeps_querying_chargebee_for_subscriptions_if_next_offset_returned(self):
+        response_1 = mock.Mock()
+        response_1.json.return_value = {
+            'list': [],
+            'next_offset': '["1522747277000", "1522747277010"]'
+        }
+        response_2 = mock.Mock()
+        response_2.json.return_value = {
+            'list': [],
+            'next_offset': '["1522747277010", "1522747277020"]'
+        }
+        response_3 = mock.Mock()
+        response_3.json.return_value = {
+            'list': [],
+        }
+        self.requests_get.side_effect = [response_1, response_2, response_3]
+
+        self.chargebee_client.get_all_subscriptions()
+
+        self.requests_get.assert_has_calls([
+            mock.call(
+                self.chargebee_client.api_url + '/subscriptions',
+                auth=self.chargebee_client.auth,
+                params={}
+            ),
+            mock.call(
+                self.chargebee_client.api_url + '/subscriptions',
+                auth=self.chargebee_client.auth,
+                params={'offset': '["1522747277000", "1522747277010"]'}
+            ),
+            mock.call(
+                self.chargebee_client.api_url + '/subscriptions',
+                auth=self.chargebee_client.auth,
+                params={'offset': '["1522747277010", "1522747277020"]'}
+            ),
+        ])
+
+    def test_returns_all_subscriptions(self):
+        response_1 = mock.Mock()
+        response_1.json.return_value = {
+            'list': [{'subscription': {'id': 1}}],
+            'next_offset': '["1522747277000", "1522747277010"]'
+        }
+        response_2 = mock.Mock()
+        response_2.json.return_value = {
+            'list': [{'subscription': {'id': 2}}],
+            'next_offset': '["1522747277010", "1522747277020"]'
+        }
+        response_3 = mock.Mock()
+        response_3.json.return_value = {
+            'list': [{'subscription': {'id': 3}}],
+        }
+        self.requests_get.side_effect = [response_1, response_2, response_3]
+
+        ret = self.chargebee_client.get_all_subscriptions()
+
+        self.assertCountEqual(ret, [{'subscription': {'id': 1}},
+                                    {'subscription': {'id': 2}},
+                                    {'subscription': {'id': 3}}])
+
+    def test_preserves_parameters_when_doing_multiple_requests_to_chargebee(self):
+        response_1 = mock.Mock()
+        response_1.json.return_value = {
+            'list': [],
+            'next_offset': '["1522747277000", "1522747277010"]'
+        }
+        response_2 = mock.Mock()
+        response_2.json.return_value = {
+            'list': [],
+            'next_offset': '["1522747277010", "1522747277020"]'
+        }
+        response_3 = mock.Mock()
+        response_3.json.return_value = {
+            'list': [],
+        }
+        self.requests_get.side_effect = [response_1, response_2, response_3]
+
+        self.chargebee_client.get_all_subscriptions({'status[is]': 'active'})
+
+        self.requests_get.assert_has_calls([
+            mock.call(
+                self.chargebee_client.api_url + '/subscriptions',
+                auth=self.chargebee_client.auth,
+                params={'status[is]': 'active'}
+            ),
+            mock.call(
+                self.chargebee_client.api_url + '/subscriptions',
+                auth=self.chargebee_client.auth,
+                params={'status[is]': 'active', 'offset': '["1522747277000", "1522747277010"]'}
+            ),
+            mock.call(
+                self.chargebee_client.api_url + '/subscriptions',
+                auth=self.chargebee_client.auth,
+                params={'status[is]': 'active', 'offset': '["1522747277010", "1522747277020"]'}
+            ),
+        ])
