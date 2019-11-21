4215d00789fdc2842915a724541e021005ad8f38
Bono de Visser <bono@byte.nl>
2019-11-21 18:26:14 +0100

    Deduplicate client code

--- chargebee_byte/client.py
+++ chargebee_byte/client.py
@@ -9,38 +9,31 @@ class Client(object):
         self.auth = requests.auth.HTTPBasicAuth(api_key, '')
         self.api_url = 'https://{}.chargebee.com/api/v2'.format(site)
 
-    def get_paginated_subscriptions(self, parameters=None):
-        request = SubscriptionRequest(parameters)
+    def _get_paginated_objects(self, request):
         response = requests.get(self.api_url + request.path, auth=self.auth, params=request.data)
         response.raise_for_status()
         return response.json()
+
+    def _get_all_objects(self, paginated_func, parameters):
+        ret = {'next_offset': ''}
+        objects = []
+
+        while 'next_offset' in ret:
+            new_parameters = deepcopy(parameters) or {}
+            new_parameters['offset'] = ret['next_offset']
+            ret = paginated_func(new_parameters)
+            objects += ret['list']
+
+        return objects
+
+    def get_paginated_subscriptions(self, parameters=None):
+        return self._get_paginated_objects(SubscriptionRequest(parameters))
 
     def get_all_subscriptions(self, parameters=None):
-        ret = {'next_offset': ''}
-        subscriptions = []
-
-        while 'next_offset' in ret:
-            new_parameters = deepcopy(parameters) or {}
-            new_parameters['offset'] = ret['next_offset']
-            ret = self.get_paginated_subscriptions(new_parameters)
-            subscriptions += ret['list']
-
-        return subscriptions
+        return self._get_all_objects(self.get_paginated_subscriptions, parameters)
 
     def get_paginated_customers(self, parameters=None):
-        request = CustomerRequest(parameters)
-        response = requests.get(self.api_url + request.path, auth=self.auth, params=request.data)
-        response.raise_for_status()
-        return response.json()
+        return self._get_paginated_objects(CustomerRequest(parameters))
 
     def get_all_customers(self, parameters=None):
-        ret = {'next_offset': ''}
-        customers = []
-
-        while 'next_offset' in ret:
-            new_parameters = deepcopy(parameters) or {}
-            new_parameters['offset'] = ret['next_offset']
-            ret = self.get_paginated_customers(new_parameters)
-            customers += ret['list']
-
-        return customers
+        return self._get_all_objects(self.get_paginated_customers, parameters)
