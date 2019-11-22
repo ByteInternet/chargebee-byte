3ea876eea1493ce029671495fbbceeb09b0e630d
Bono de Visser <bono@byte.nl>
2019-11-22 13:34:47 +0100

    Fix test name

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -7,5 +7,5 @@ class TestClient(TestBase):
     def test_sets_api_url_to_provided_site(self):
         self.assertEqual(self.chargebee_client.api_url, 'https://my_site.chargebee.com/api/v2')
 
-    def test_sets_auth_to_hhtp_basic_auth_method_with_api_key_as_username(self):
+    def test_sets_auth_to_http_basic_auth_method_with_api_key_as_username(self):
         self.assertEqual(self.chargebee_client.auth, requests.auth.HTTPBasicAuth('my_api_key', ''))
