70e8717af815cda01f98e832a490b7618bdba4f1
Bono de Visser <bono@byte.nl>
2019-11-22 11:39:49 +0100

    Rewrite paginated tests

--- tests/client/test_get_paginated_customers.py
+++ tests/client/test_get_paginated_customers.py
@@ -1,48 +1,22 @@
-from unittest import mock
-
-from chargebee_byte.requests import CustomerRequest
 from tests.test import TestBase
 
 
 class TestGetPaginatedCustomers(TestBase):
     def setUp(self):
-        super().setUp()
-        self.requests_get = self.set_up_patch('chargebee_byte.client.requests.get')
+        super(TestGetPaginatedCustomers, self).setUp()
+        self.get_paginated_objects = self.set_up_patch('chargebee_byte.client.Client._get_paginated_objects')
+        self.customer_request = self.set_up_patch('chargebee_byte.client.CustomerRequest')
 
-    def test_calls_chargebee_api_with_correct_parameters(self):
-        self.chargebee_client.get_paginated_customers()
-
-        self.requests_get.assert_called_once_with(
-            self.chargebee_client.api_url + '/customers',
-            auth=self.chargebee_client.auth,
-            params={}
-        )
-
-    def test_returns_json_result_from_requests_get_call(self):
-        ret = self.chargebee_client.get_paginated_customers()
-
-        self.assertEqual(self.requests_get.return_value.json(), ret)
-
-    def test_calls_chargebee_api_with_provided_parameters(self):
+    def test_calls_get_paginated_objects_with_correct_parameters(self):
         self.chargebee_client.get_paginated_customers(parameters={'sort_by[asc]': 'first_name'})
 
-        self.requests_get.assert_called_once_with(
-            self.chargebee_client.api_url + '/customers',
-            auth=self.chargebee_client.auth,
-            params={'sort_by[asc]': 'first_name'}
-        )
+        self.get_paginated_objects.assert_called_once_with(self.customer_request({'sort_by[asc]': 'first_name'}))
 
-    def test_raises_value_error_if_unknown_parameter_supplied(self):
-        parameter = 'banaan[henk]'
-        self.assertNotIn(parameter, CustomerRequest().allowed_parameters)
-
-        with self.assertRaises(ValueError):
-            self.chargebee_client.get_paginated_customers(parameters={parameter: 'bonobo'})
-
-    def test_calls_raise_for_status_on_requests_response(self):
-        response = mock.Mock()
-        self.requests_get.return_value = response
-
+    def test_calls_get_paginated_objects_with_empty_parameters(self):
         self.chargebee_client.get_paginated_customers()
 
-        response.raise_for_status.assert_called_once_with()
+        self.get_paginated_objects.assert_called_once_with(self.customer_request({}))
+
+
+
+
--- tests/client/test_get_paginated_subscriptions.py
+++ tests/client/test_get_paginated_subscriptions.py
@@ -1,48 +1,18 @@
-from unittest import mock
-
-from chargebee_byte.requests import SubscriptionRequest
 from tests.test import TestBase
 
 
 class TestGetPaginatedSubscriptions(TestBase):
     def setUp(self):
-        super().setUp()
-        self.requests_get = self.set_up_patch('chargebee_byte.client.requests.get')
+        super(TestGetPaginatedSubscriptions, self).setUp()
+        self.get_paginated_objects = self.set_up_patch('chargebee_byte.client.Client._get_paginated_objects')
+        self.subscription_request = self.set_up_patch('chargebee_byte.client.SubscriptionRequest')
 
-    def test_calls_chargebee_api_with_correct_parameters(self):
+    def test_calls_get_paginated_objects_with_correct_parameters(self):
+        self.chargebee_client.get_paginated_subscriptions(parameters={'sort_by[asc]': 'first_name'})
+
+        self.get_paginated_objects.assert_called_once_with(self.subscription_request({'sort_by[asc]': 'first_name'}))
+
+    def test_calls_get_paginated_objects_with_empty_parameters(self):
         self.chargebee_client.get_paginated_subscriptions()
 
-        self.requests_get.assert_called_once_with(
-            self.chargebee_client.api_url + '/subscriptions',
-            auth=self.chargebee_client.auth,
-            params={}
-        )
-
-    def test_returns_json_result_from_requests_get_call(self):
-        ret = self.chargebee_client.get_paginated_subscriptions()
-
-        self.assertEqual(self.requests_get.return_value.json(), ret)
-
-    def test_calls_chargebee_api_with_provided_parameters(self):
-        self.chargebee_client.get_paginated_subscriptions(parameters={'status[is]': 'active'})
-
-        self.requests_get.assert_called_once_with(
-            self.chargebee_client.api_url + '/subscriptions',
-            auth=self.chargebee_client.auth,
-            params={'status[is]': 'active'}
-        )
-
-    def test_raises_value_error_if_unknown_parameter_supplied(self):
-        parameter = 'banaan[henk]'
-        self.assertNotIn(parameter, SubscriptionRequest().allowed_parameters)
-
-        with self.assertRaises(ValueError):
-            self.chargebee_client.get_paginated_subscriptions(parameters={parameter: 'bonobo'})
-
-    def test_calls_raise_for_status_on_requests_response(self):
-        response = mock.Mock()
-        self.requests_get.return_value = response
-
-        self.chargebee_client.get_paginated_subscriptions()
-
-        response.raise_for_status.assert_called_once_with()
+        self.get_paginated_objects.assert_called_once_with(self.subscription_request({}))
